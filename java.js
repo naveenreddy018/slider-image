5b6a35d5b938595e5b7668507cbf2eedea4633e6
naveenreddy018 <naveenreddy310744@gmail.com>
2024-09-12 12:08:27 +0530

    first

---
+++ java.js
@@ -0,0 +1,72 @@
+//taSk 1
+/* function show_message(){
+    console.log(" \"  "+  "hello welcome to my web page" + " !" + " \" " )
+}
+show_message() */
+
+//task2 
+/* function counttoten(){
+    for(i=1;i<=10;i++){
+        console.log(i)
+    }
+}
+counttoten() */
+//task3
+/* function mutipleoftable(){
+     num = 2
+     for(i=1;i<=10;i++){
+        console.log(num + " x" + i + " = " + num*i)
+     }
+}
+mutipleoftable() */
+
+//task4
+/*  function oddnumbers(){
+    for(i=1;i<=10;i++){
+       if(i % 2 != 0){
+        console.log(`${i} \t odd number`)
+       }
+    }
+}
+oddnumbers()  */
+//task5
+/* function countdown(){
+    for(i=5;i>0;i--){
+        console.log(i)
+    }
+}
+countdown() */
+//task6
+/* function greetuser(){
+    console.log( " \" " +  "Good day, user" + " !" + "\" ")
+}
+greetuser() */
+//task7
+/*  function evennumbers(){
+    for(i=1;i<=10;i++){
+        if(i%2 == 0){
+            console.log(i + " \t" + " EVEN NUMBER ")
+        }
+    }
+} 
+evennumbers() */
+//task 8
+/* function  farewell(){
+    console.log( "\" "  + "Thank you for visiting, goodbye" + " !" + " \" ")
+}
+farewell() */
+//task 9 
+/*  function squares(){
+    for(i=1;i<=5;i++){
+        squares = i**2
+      console.log(`${i} square is = ${squares} `)
+    }
+}
+squares()  */
+//task10
+/*  function repeatwelcome(){
+    for(i=1;i<=3;i++){
+        console.log(" \" " + "Welcome" + " !" + " \" ")
+    }
+}
+repeatwelcome()  */
